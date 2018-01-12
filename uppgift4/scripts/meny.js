6818a409e974c452f7f876b6f6f9a7822ea42feb
Lorenz Gerber <lorenz.gerber@lablicate.com>
2018-01-12 16:50:54 +0100

    working on visibility

--- uppgift4/scripts/meny.js
+++ uppgift4/scripts/meny.js
@@ -3,6 +3,7 @@ $(document).ready(generateMenu);
 function generateMenu() {
   addIdsToRecipes();
   addMenu();
+  setRecipeVisible('recipe_1');
 }
 
 function addIdsToRecipes(){
@@ -13,23 +14,37 @@ function addIdsToRecipes(){
   }
 }
 
+function setRecipeVisible(postId){
+  setAllRecipeInvisible();
+  var post = document.getElementById(postId);
+  post.style.display = 'block';
+}
+
 function addMenu(getNumberOfRecipes){
   var menu = document.createElement('ul');
   var primaryContent = document.getElementById('primarycontent');
   var posts = primaryContent.getElementsByClassName('post');
-  
+
   for( var i = 0; i < posts.length; i++ ){
     var menuItem = document.createElement('li');
     var link = document.createElement('a');
-    link.href = '#recipe_' + i;
+    //link.href = '#recipe_' + i;
     link.textContent = posts[i].getElementsByTagName("h4")[0].innerHTML;
+    var currentId = 'recipe_' + i;
+    link.addEventListener("click", function() { alert("shit");}, false);
+
     menuItem.appendChild(link);
     menu.appendChild(menuItem);
     var receptmeny = document.getElementById('receptmeny');
     var contentArea = receptmeny.getElementsByClassName('contentarea')[0];
     contentArea.appendChild(menu);
   }
+}
 
-
-
+function setAllRecipeInvisible(){
+  var primaryContent = document.getElementById('primarycontent');
+  var posts = primaryContent.getElementsByClassName('post');
+  for( var i = 0; i < posts.length; i++ ){
+    posts[i].style.display = 'none';
+  }
 }
